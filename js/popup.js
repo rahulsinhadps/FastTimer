dbd47daf3222005687debfe7146ba2983a07b6a9
Rahul <rsinha@fastenal.com>
2017-09-15 12:35:02 +0530

    Refactoring

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     initializeSubmitDataClick();
     chrome.storage.sync.get("empId", function (response) {
         $("#empId").val(response.empId);
+        sleep(50);
         $("#submitEmpId").click();
     });
 });
@@ -30,54 +31,66 @@ function initializeSubmitDataClick() {
         });
         var input = {fields: fields};
         chrome.runtime.sendMessage(input, function (response) {
-            var result = JSON.parse(response.d);
-            console.log(result.SwipeRecord);
-            $("#timeLogTable").html("");
-            var lastIn = null;
-            var totalMinutes = 0;
-            $.each(result.SwipeRecord, function () {
-                var armyTime = moment(this.swipeTime, ["h:mm A"]).format("HH:mm");
-                var logDate = $("#dateForLog").val() + " " + armyTime + ":00";
-                if (lastIn == null && this.swipeInOut == "In") {
-                    lastIn = logDate;
-                }
-                if (this.swipeInOut == "Out" && lastIn != null) {
-                    var output = moment.utc(moment(logDate, "MM-DD-YYYY HH:mm:ss").diff(moment(lastIn, "MM-DD-YYYY HH:mm:ss"))).format("HH:mm");
-                    var outputArray = output.split(":");
-                    totalMinutes += (parseInt(outputArray[0]) * 60) + parseInt(outputArray[1]);
-                    lastIn = null;
-                }
-                var $tr = $("<tr><td>" + this.swipeInOut + "</td><td>" + armyTime + "</td></td></tr>");
-                $("#timeLogTable").append($tr);
-
-            });
-            if (lastIn != null) {
-                var lastLog = new Date();
-                var lastInDate = (moment(lastIn, "MM-DD-YYYY HH:mm:ss")).toDate();
-                if ((new Date).getDate() - lastInDate.getDate() != 0) {
-                    lastLog.setHours(0, 0, 0);
-                }
-                var output = moment.utc(moment(lastLog).diff(moment(lastIn, "MM-DD-YYYY HH:mm:ss"))).format("HH:mm");
-                var outputArray = output.split(":");
-                totalMinutes += (parseInt(outputArray[0]) * 60) + parseInt(outputArray[1]);
-                lastIn = null;
-
-            }
-            $totalLogTime = $("<div>" + Math.floor(totalMinutes / 60) + " hours " + totalMinutes % 60 + " minutes </div>");
-
-            if (totalMinutes == 0) {
-                $("#swipeTable").hide();
-            } else {
-                $("#swipeTable").show();
-            }
-
-            $(".js-chip").html($totalLogTime);
-            $("#chipDiv").show();
-
+            calculateTimeLogAndSetTableData(response);
         });
 
         event.preventDefault();
 
     });
+}
+
+function calculateTimeLogAndSetTableData(response) {
+    var result = JSON.parse(response.d);
+    $("#timeLogTable").html("");
+    var lastIn = null;
+    var totalMinutes = 0;
+    var resultList = result.SwipeRecord;
+    $.each(resultList, function () {
+        var armyTime = moment(this.swipeTime, ["h:mm A"]).format("HH:mm");
+        var logDate = $("#dateForLog").val() + " " + armyTime + ":00";
+        if (lastIn == null && this.swipeInOut == "In") {
+            lastIn = logDate;
+        }
+        if (this.swipeInOut == "Out" && lastIn != null) {
+            var output = moment.utc(moment(logDate, "MM-DD-YYYY HH:mm:ss").diff(moment(lastIn, "MM-DD-YYYY HH:mm:ss"))).format("HH:mm");
+            var outputArray = output.split(":");
+            totalMinutes += (parseInt(outputArray[0]) * 60) + parseInt(outputArray[1]);
+            lastIn = null;
+        }
+        var $tr = $("<tr><td>" + this.swipeInOut + "</td><td>" + armyTime + "</td></td></tr>");
+        $("#timeLogTable").append($tr);
+
+    });
+    if (lastIn != null) {
+        var lastLog = new Date();
+        var lastInDate = (moment(lastIn, "MM-DD-YYYY HH:mm:ss")).toDate();
+        if ((new Date).getDate() - lastInDate.getDate() != 0) {
+            lastLog.setHours(0, 0, 0);
+        }
+        var output = moment.utc(moment(lastLog).diff(moment(lastIn, "MM-DD-YYYY HH:mm:ss"))).format("HH:mm");
+        var outputArray = output.split(":");
+        totalMinutes += (parseInt(outputArray[0]) * 60) + parseInt(outputArray[1]);
+        lastIn = null;
+
+    }
+    var $totalLogTime = $("<div>" + Math.floor(totalMinutes / 60) + " hours " + totalMinutes % 60 + " minutes </div>");
+
+    if (totalMinutes == 0) {
+        $("#swipeTable").hide();
+    } else {
+        $("#swipeTable").show();
+    }
+
+    $(".js-chip").html($totalLogTime);
+    $("#chipDiv").show();
+}
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+    }
 }
 
