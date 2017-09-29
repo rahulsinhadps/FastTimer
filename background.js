f397f4ccd941f018783d1219af11ace639cd8729
Aman Kumar <aman943196@gmail.com>
2017-09-30 00:53:14 +0530

    Still need to implement Callback in a Loop

--- background.js
+++ background.js
@@ -1,27 +1,30 @@
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (response, sender, sendResponse) {
     var employeeId = parseInt(response.fields.empId);
     var date = response.fields.dateForLog;
-    var inp = {SelectedDate: date, empid: employeeId };
+    var javascriptObjectForInput = {SelectedDate: date, empid: employeeId};
+
     if (employeeId != null && employeeId != "") {
         utility.saveEmployeeId(employeeId);
     }
+
     $.ajax({
         type: "POST",
         async: false,
         url: "https://itportaltimer.azurewebsites.net/attendancecalendar/pages/Attendancecalendar.aspx/getDailyPunchRecords",
-        data: JSON.stringify(inp),
-        success: function(data) {
+        data: JSON.stringify(javascriptObjectForInput),
+        success: function (data) {
             var dt = moment("12:15 AM", ["h:mm A"]).format("HH:mm");
             sendResponse(data);
         },
+
         contentType: "application/json; charset=utf-8"
     });
 });
 
 var utility = {
-    saveEmployeeId : function(empId) {
+    saveEmployeeId: function (empId) {
         // Save it using the Chrome extension storage API.
-        chrome.storage.sync.set({'empId': empId}, function() {
+        chrome.storage.sync.set({'empId': empId}, function () {
             console.log("Saved employeeId");
         });
     }
--- js/popup.js
+++ js/popup.js
@@ -10,171 +10,182 @@ $(document).ready(function () {
 });
 
 var timeLogger = {
-    initializeDatePicker: function () {
-        var $datepicker = $('.datepicker').pickadate({
-            selectMonths: true, // Creates a dropdown to control month
-            selectYears: 15, // Creates a dropdown of 15 years to control year,
-            today: 'Today',
-            clear: 'Clear',
-            close: 'Ok',
-            closeOnSelect: true,
-            format: 'mm-dd-yyyy'// Close upon selecting a date,
-        });
+        initializeDatePicker: function () {
+            var $datepicker = $('.datepicker').pickadate({
+                selectMonths: true, // Creates a dropdown to control month
+                selectYears: 15, // Creates a dropdown of 15 years to control year,
+                today: 'Today',
+                clear: 'Clear',
+                close: 'Ok',
+                closeOnSelect: true,
+                format: 'mm-dd-yyyy'// Close upon selecting a date,
+            });
 
-        var picker = $datepicker.pickadate('picker');
+            var picker = $datepicker.pickadate('picker');
 
-        picker.set('select', new Date());
-    },
+            picker.set('select', new Date());
+        },
 
-    initializeSubmitDataClick: function () {
-        $("#submitEmpId").click(function (event) {
-            var $employeeIdForm = $("#empForm");
+        initializeSubmitDataClick: function () {
+            $("#submitEmpId").click(function (event) {
+                var input = {fields: timeLogger.createInputToGetTime()};
+
+                chrome.runtime.sendMessage(input, function (response) {
+                    var totalTimeLoggedInADay = timeLogger.calculateTimeLogAndSetTableData(response);
+
+                    $("#chipDiv").hide();
+
+                    if (totalTimeLoggedInADay != null && totalTimeLoggedInADay != 0) {
+                        timeLogger.setCalculatedTotalTimeToPage(totalTimeLoggedInADay);
+                    } else {
+                        $("#swipeTable").hide();
+                    }
+
+                });
+
+                timeLogger.initializeTotalTimeCalculationOfWeek();
+                event.preventDefault();
+            });
+        },
+
+        createInputToGetTime: function () {
             var fields = {};
-            $employeeIdForm.find(":input").each(function () {
+
+            $("#empForm").find(":input").each(function () {
                 fields[this.name] = $(this).val();
             });
 
-            var input = {fields: fields};
+            return fields;
+        },
 
-            chrome.runtime.sendMessage(input, function (response) {
-                var totalTimeLoggedInADay = timeLogger.calculateTimeLogAndSetTableData(response);
-                $("#chipDiv").hide();
+        initializeTotalTimeCalculationOfWeek: function () {
+            var $today = new Date("09/29/2017");
+            var calculateWeekDay = $today.getDay();
+            var totalLogTimeInWeek = 0;
+            var counterToDecrementDate = 0;
+            var todayLoggedTime = 0;
+            var validNoOfDays = 5;
 
-                if (totalTimeLoggedInADay != null && totalTimeLoggedInADay != 0) {
-                    timeLogger.setCalculatedTotalTimeToPage(totalTimeLoggedInADay);
-                } else {
-                    $("#swipeTable").hide();
+            if (calculateWeekDay === 6 || calculateWeekDay === 0) {
+                $("#chipDivWeekend").show();
+            } else {
+                for (var i = calculateWeekDay; i > 0; i--) {
+                    var fields = timeLogger.createInputToGetTime();
+                    var dateToCompute = new Date();
+                    dateToCompute.setDate($today.getDate() - counterToDecrementDate);
+                    fields.dateForLog = dateToCompute;
+                    var input = {fields: fields};
+
+                    chrome.runtime.sendMessage(input, function (response) {
+                        var totalTimeLoggedInPassedDate = timeLogger.calculateTimeLogAndSetTableData(response);
+                        totalLogTimeInWeek = totalLogTimeInWeek + totalTimeLoggedInPassedDate;
+
+                        if (counterToDecrementDate === 0) {
+                            todayLoggedTime = totalTimeLoggedInPassedDate;
+                            timeLogger.setCalculatedTotalTimeToPage(totalTimeLoggedInPassedDate);
+                        }
+
+                        if (totalTimeLoggedInPassedDate == null || totalTimeLoggedInPassedDate == undefined || totalTimeLoggedInPassedDate == 0) {
+                            validNoOfDays--;
+                        }
+
+                    });
                 }
-            });
-
-            event.preventDefault();
-        });
-    },
-
-    initializeTotalTimeCalculationOfWeek: function () {
-        var $today = new Date();
-        var calculateWeekDay = $today.getDay();
-        var totalLogTimeInWeek = 0;
-        var counterToDecrementDate = 0;
-        var todayLoggedTime = 0;
-        var validNoOfDays = 5;
-
-        if (calculateWeekDay === 6 || calculateWeekDay === 0) {
-            $("#chipDivWeekend").show();
-        } else {
-            for (var i = calculateWeekDay; i > 0; i--) {
-                var dateToLog = $today.getDate() - counterToDecrementDate;
-                var $employeeIdForm = $("#empForm");
-                var fields = {};
-                $employeeIdForm.find(":input").each(function () {
-                    fields[this.name] = $(this).val();
-                });
-
-                var input = {fields: fields};
-
-                chrome.runtime.sendMessage(input, function (response) {
-                    var totalTimeLoggedInPassedDate = timeLogger.calculateTimeLogAndSetTableData(response);
-                    totalLogTimeInWeek = totalLogTimeInWeek + totalTimeLoggedInPassedDate;
-
-                    if (counterToDecrementDate === 0) {
-                        todayLoggedTime = totalTimeLoggedInPassedDate;
-                        timeLogger.setCalculatedTotalTimeToPage(totalTimeLoggedInPassedDate);
-                    }
-
-                    if (totalTimeLoggedInPassedDate == null || totalTimeLoggedInPassedDate == undefined || totalTimeLoggedInPassedDate == 0) {
-                        validNoOfDays--;
-                    }
-
-                });
 
                 counterToDecrementDate++;
             }
 
-            if (calculateWeekDay === 5) {
-                timeLogger.showHoursRemainingToClock40(validNoOfDays, totalLogTimeInWeek, todayLoggedTime);
-            }
-
+            timeLogger.showHoursRemainingToClock40(validNoOfDays, totalLogTimeInWeek, todayLoggedTime);
         }
     },
 
-    showHoursRemainingToClock40: function (validNoOfDays, totalLogTimeInWeekDone, todayLoggedTime) {
-        var totalHoursCompulsoryToClock = validNoOfDays * 8;
-        var totalMinutesCompulsoryToClock = totalHoursCompulsoryToClock * 60;
-        var remainingMinutesCompulsoryToClock = totalMinutesCompulsoryToClock - totalLogTimeInWeekDone;
+    showHoursRemainingToClock40
+:
 
-        if (remainingMinutesCompulsoryToClock > 0) {
-            timeLogger.setCalculatedTotalTimeReamainingToPage(remainingMinutesCompulsoryToClock);
+function (validNoOfDays, totalLogTimeInWeekDone, todayLoggedTime) {
+    var totalHoursCompulsoryToClock = validNoOfDays * 8;
+    var totalMinutesCompulsoryToClock = totalHoursCompulsoryToClock * 60;
+    var remainingMinutesCompulsoryToClock = totalMinutesCompulsoryToClock - totalLogTimeInWeekDone;
+
+    if (remainingMinutesCompulsoryToClock > 0) {
+        timeLogger.setCalculatedTotalTimeReamainingToPage(remainingMinutesCompulsoryToClock);
+    } else {
+        if (todayLoggedTime < 330) {
+            $("#chipDivCompleteMinHours").show();
         } else {
-            if (todayLoggedTime < 330) {
-                $("#chipDivCompleteMinHours").show();
-            }
+            alert("YOU SHOULD GO");
         }
-    },
+    }
+}
 
-    calculateTimeLogAndSetTableData: function (response) {
-        var result = JSON.parse(response.d);
-        $("#timeLogTable").html("");
-        var lastIn = null;
-        var totalMinutes = 0;
-        var resultList = result.SwipeRecord;
+,
 
-        $.each(resultList, function () {
-            var armyTime = moment(this.swipeTime, ["h:mm A"]).format("HH:mm");
-            var logDate = $("#dateForLog").val() + " " + armyTime + ":00";
+calculateTimeLogAndSetTableData: function (response) {
+    var result = JSON.parse(response.d);
+    $("#timeLogTable").html("");
+    var lastIn = null;
+    var totalMinutes = 0;
+    var resultList = result.SwipeRecord;
 
-            if (lastIn == null && this.swipeInOut == "In") {
-                lastIn = logDate;
-            }
+    $.each(resultList, function () {
+        var armyTime = moment(this.swipeTime, ["h:mm A"]).format("HH:mm");
+        var logDate = $("#dateForLog").val() + " " + armyTime + ":00";
 
-            if (this.swipeInOut == "Out" && lastIn != null) {
-                var output = moment.utc(moment(logDate, "MM-DD-YYYY HH:mm:ss").diff(moment(lastIn, "MM-DD-YYYY HH:mm:ss"))).format("HH:mm");
-                var outputArray = output.split(":");
-                totalMinutes += (parseInt(outputArray[0]) * 60) + parseInt(outputArray[1]);
-                lastIn = null;
-            }
+        if (lastIn == null && this.swipeInOut == "In") {
+            lastIn = logDate;
+        }
 
-            var $tr = $("<tr><td>" + this.swipeInOut + "</td><td>" + armyTime + "</td></td></tr>");
-            $("#timeLogTable").append($tr);
-        });
-
-        if (lastIn != null) {
-            var lastLog = new Date();
-            var lastInDate = (moment(lastIn, "MM-DD-YYYY HH:mm:ss")).toDate();
-
-            if ((new Date).getDate() - lastInDate.getDate() != 0) {
-                lastLog.setHours(0, 0, 0);
-            }
-
-            var output = moment.utc(moment(lastLog).diff(moment(lastIn, "MM-DD-YYYY HH:mm:ss"))).format("HH:mm");
+        if (this.swipeInOut == "Out" && lastIn != null) {
+            var output = moment.utc(moment(logDate, "MM-DD-YYYY HH:mm:ss").diff(moment(lastIn, "MM-DD-YYYY HH:mm:ss"))).format("HH:mm");
             var outputArray = output.split(":");
             totalMinutes += (parseInt(outputArray[0]) * 60) + parseInt(outputArray[1]);
             lastIn = null;
-
         }
 
-        return totalMinutes;
+        var $tr = $("<tr><td>" + this.swipeInOut + "</td><td>" + armyTime + "</td></td></tr>");
+        $("#timeLogTable").append($tr);
+    });
 
-    },
+    if (lastIn != null) {
+        var lastLog = new Date();
+        var lastInDate = (moment(lastIn, "MM-DD-YYYY HH:mm:ss")).toDate();
 
-    setCalculatedTotalTimeToPage: function (totalMinutes) {
-        var $totalLogTime = $("<div>" + Math.floor(totalMinutes / 60) + " hours " + totalMinutes % 60 + " minutes </div>");
-
-        if (totalMinutes == 0) {
-            $("#swipeTable").hide();
-        } else {
-            $("#swipeTable").show();
+        if ((new Date).getDate() - lastInDate.getDate() != 0) {
+            lastLog.setHours(0, 0, 0);
         }
 
-        $(".js-chip").html($totalLogTime);
-        $("#chipDiv").show();
-    },
+        var output = moment.utc(moment(lastLog).diff(moment(lastIn, "MM-DD-YYYY HH:mm:ss"))).format("HH:mm");
+        var outputArray = output.split(":");
+        totalMinutes += (parseInt(outputArray[0]) * 60) + parseInt(outputArray[1]);
+        lastIn = null;
 
-    setCalculatedTotalTimeReamainingToPage: function (totalMinutes) {
-        var $totalRemainingInWeek = $("<div>" + Math.floor(totalMinutes / 60) + " hours " + totalMinutes % 60 + " minutes </div>");
-
-        $(".js2-chip").html($totalRemainingInWeek);
-        $("#chip2Div").show();
     }
 
-};
+    return totalMinutes;
+
+}
+,
+
+setCalculatedTotalTimeToPage: function (totalMinutes) {
+    var $totalLogTime = $("<div>" + Math.floor(totalMinutes / 60) + " hours " + totalMinutes % 60 + " minutes </div>");
+
+    if (totalMinutes == 0) {
+        $("#swipeTable").hide();
+    } else {
+        $("#swipeTable").show();
+    }
+
+    $(".js-chip").html($totalLogTime);
+    $("#chipDiv").show();
+}
+,
+
+setCalculatedTotalTimeReamainingToPage: function (totalMinutes) {
+    var $totalRemainingInWeek = $("<div>" + Math.floor(totalMinutes / 60) + " hours " + totalMinutes % 60 + " minutes </div>");
+
+    $(".js2-chip").html($totalRemainingInWeek);
+    $("#chip2Div").show();
+}
+}
+;
 
